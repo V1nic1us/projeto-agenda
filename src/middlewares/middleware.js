c25049b1fd22855853304e00d7c62ebf9152af2b
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-04-11 13:18:49 -0400

    added 404 error and application error middleware

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -12,9 +12,17 @@ exports.checkCsrfError = (error ,request ,response, next) => {
     if (error && error.code == 'EBADCSRFTOKEN') {
         return response.render('404');
     }
+    next();
 }
 
 exports.csrfMiddleware = (request ,response, next) => {
     response.locals.csrfToken = request.csrfToken();
+    next();
+}
+
+exports.errorApplication = (error, request, response, next) => {
+    if (error) {
+        return response.render('error');
+    }
     next();
 }
