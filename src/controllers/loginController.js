4d91ddbc65ff737fd0e98d24286718a537e37360
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-04-12 09:34:03 -0400

    register user to controller

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,3 +1,27 @@
+const { async } = require('regenerator-runtime');
+const Login = require('../models/LoginModel');
+
 exports.index = (request, response) => {
-    response.render('login');
+    return response.render('login');
+}
+
+exports.register = async function(request, response) {
+    try {
+        const login = new Login(request.body);
+        await login.register();
+
+        if (login.errors.length > 0) {
+            request.flash('errors', login.errors);
+            request.session.save(function() {
+                return response.redirect('back');
+            });
+            return;
+        }
+        request.flash('success', 'Seu Usuario foi criado com sucesso');
+        request.session.save(function() {
+            return response.redirect('back');
+        });  
+    } catch (error) {
+        return response.render('error');
+    }   
 }
