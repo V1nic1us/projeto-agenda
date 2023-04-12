c0ecf34a89bf35ebd87d1d6044048b3a1a9a769e
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-04-12 09:39:58 -0400

    created user login model

---
+++ src/models/LoginModel.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcryptjs = require('bcryptjs');
+const LoginSchema = new mongoose.Schema({
+    email: { type: String , require: true},
+    password: { type: String , require: true}
+});
+
+const LoginModel= mongoose.model('Login', LoginSchema);
+
+class Login{
+    constructor(body) {
+        this.body = body;
+        this.errors = [];
+        this.user = null;
+    }
+
+    async register() {
+        this.valida();
+        if(this.errors.length > 0) return;
+
+        await this.userExists();
+
+        if(this.errors.length > 0) return;
+
+        try {
+            const salt = bcryptjs.genSaltSync();
+            this.body.password= bcryptjs.hashSync(this.body.password, salt);
+            this.user = await LoginModel.create(this.body);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async userExists() {
+        const user = await LoginModel.findOne({ email: this.body.email });
+        if (user) this.errors.push('Usuário ja foi cadastrado')
+    }
+
+    valida() {
+        this.cleanUp();
+        if(!validator.isEmail(this.body.email)) this.errors.push('E-mail invalido');
+        if (this.body.password.length < 3 || this.body.password.length > 50) this.errors.push('Senha invalida')
+    }
+
+    cleanUp(){
+        for (const key in this.body) {
+            if (typeof this.body[key] !== 'string') {
+                this.body[key]= '';
+                
+            }
+        }
+
+        this.body= {
+            email: this.body.email,
+            password: this.body.password
+        }
+    }
+}
+
+module.exports = Login;
