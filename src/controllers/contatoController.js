a1d44314f5a3df262d0591c1f5e7696632c2cd96
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-04-17 21:10:45 -0400

    feat: added contact creation controller

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -1,3 +1,25 @@
+const Contato = require('../models/ContatoModel');
+
 exports.index = (request, response) => {
     response.render('contato');
+}
+
+exports.register = async(request, response) => {
+    try {
+        const contato = new Contato(request.body);
+        await contato.register();
+        if (contato.errors.length > 0) {
+            request.flash('errors', contato.errors);
+            request.session.save(() => response.redirect('back'));
+            return;
+        }
+
+        request.flash('success', 'Contato Registrado com sucesso');
+        request.session.save(() => response.redirect('back'));
+        return;   
+    } catch (error) {
+        console.log(error);
+        return response.render('error');
+    }
+    
 }
