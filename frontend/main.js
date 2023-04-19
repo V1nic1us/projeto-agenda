f04ddd7607f9456d94d56be0ea7d1bce06401aef
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-04-19 14:56:18 -0400

    feat: import login modules and form classes

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,9 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import './assets/css/style.css';
 
-console.log('Entrou no main');
+import Login from './modules/Login';
+
+const login = new Login('.form-login');
+const cadastro = new Login('.form-cadastro');
+login.init();
+cadastro.init();
